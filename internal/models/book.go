55ba0362ce05fc7b974c0bef4de6d1b6fffdc02a
semicolon guy <itworksnavneet@gmail.com>
2026-03-23 16:51:25 +0530

    add Book model

--- internal/models/book.go
+++ internal/models/book.go
@@ -1,4 +1,13 @@
 package models
 
-func main() {
+import "time"
+
+type Book struct {
+	ID            int       `json:"id"`
+	Title         string    `json:"title"`
+	Author        string    `json:"author"`
+	Genre         string    `json:"genre"`
+	PublishedYear int       `json:"published_year"`
+	Available     bool      `json:"available"`
+	CreatedAt     time.Time `json:"created_at"`
 }
